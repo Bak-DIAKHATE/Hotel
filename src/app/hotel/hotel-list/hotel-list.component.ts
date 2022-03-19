8f4136720a35391c9988428519aed9d6a2676069
bakary Diakhate <bakary.diakhate@hotmail.fr>
2022-03-19 18:08:54 +0100

    Liste Hotel { get et set et creation d'un filtre de listes}

--- src/app/hotel/hotel-list/hotel-list.component.ts
+++ src/app/hotel/hotel-list/hotel-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IHotel} from "../Interface/hotel";
 
 @Component({
   selector: 'app-hotel-list',
@@ -8,46 +9,64 @@ import { Component, OnInit } from '@angular/core';
 export class HotelListComponent implements OnInit {
 public title:string ="Ma liste d hotels";
 
-public hotels:any[] = [{
-  id:1,
-   name:"King-FadPalace",
+public hotels:IHotel[] = [{
+  hotelId:1,
+   hotelName:"King-FadPalace",
   imageUrl:"assets/img/hotelKingFadPalace.jpg",
   price:500,
   description:"Hotel 5 etoiles"
 },
 {
-  id:2,
-  name:"Terroubi",
+  hotelId:2,
+  hotelName:"Terroubi",
   imageUrl:"assets/img/terroubiHotel.jpg",
   price:300,
   description:"Hotel 5 etoiles"
 },
 {
-  id:3,
-  name:"Yass Hotel",
+  hotelId:3,
+  hotelName:"Yass Hotel",
   imageUrl:"assets/img/yassHotel.jpg",
   price:150,
   description:"Hotel 5 etoiles"
 },
 {
-  id:4,
-  name:"RadissonBlu",
+  hotelId:4,
+  hotelName:"RadissonBlu",
   imageUrl:"assets/img/RadissonBlu.jpg",
   price:100,
   description:"Hotel 5 etoiles"
 }];
 
 public showBadge :boolean;
-public hotelFilter ='mot';
+private _hotelFilter = 'mot';
+
+public filteredHotels : IHotel[]=[];
 
   constructor() {
     this.showBadge = false;
   }
-
+  ngOnInit(): void {
+    this.filteredHotels =this.hotels;
+    this.hotelFilter = 'mot';
+  }
   public toggleIsNewBadge():void {
     this.showBadge = ! this.showBadge;
   }
 
-  ngOnInit(): void {
+  public get hotelFilter(){
+    return this._hotelFilter;
+  }
+  public set hotelFilter(filter:string) {
+    this._hotelFilter = filter;
+    this.filteredHotels = this.hotelFilter ? this.filterHotels(this.hotelFilter):this.hotels;
+  }
+
+  private filterHotels(criteria:string):IHotel[] {
+    criteria = criteria.toLocaleLowerCase();
+    const res = this.hotels.filter((hotel : IHotel)=>
+      hotel.hotelName.toLocaleLowerCase().indexOf(criteria)!=-1
+    );
+    return res;
   }
 }
